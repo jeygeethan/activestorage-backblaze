af5a714f2d5b3e20a39e38392d2f535f90f7fd0c
jey <geetan123321@gmail.com>
2019-07-30 21:52:25 +0530

    trial n error

--- lib/active_storage/service/backblaze_service.rb
+++ lib/active_storage/service/backblaze_service.rb
@@ -1,2 +1,20 @@
+require "fog/backblaze"
+
 class ActiveStorage::Service::BackblazeService < ActiveStorage::Service
+  def initialize(key_id:, key_token:, bucket_name:, bucket_id:)
+    @key_id = key_id
+    @key_token = key_token
+    @bucket_name = bucket_name
+    @bucket_id = bucket_id
+    @connection = Fog::Storage.new(
+      provider: 'backblaze',
+
+      b2_key_id: @key_id,
+      b2_key_token: @key_token,
+
+      b2_bucket_name: @bucket_name,
+      b2_bucket_id: @bucket_id,
+    )
+    @connection.put_object(@bucket_name, "myfile1", "THISISATESTFILE").json
+  end
 end

cb904cbb0af94f76ecafa5a355ababa5b75296df
jey <geetan123321@gmail.com>
2019-07-30 21:59:30 +0530

    trial n error

--- lib/active_storage/service/backblaze_service.rb
+++ lib/active_storage/service/backblaze_service.rb
@@ -8,13 +8,11 @@ class ActiveStorage::Service::BackblazeService < ActiveStorage::Service
     @bucket_id = bucket_id
     @connection = Fog::Storage.new(
       provider: 'backblaze',
-
       b2_key_id: @key_id,
       b2_key_token: @key_token,
-
       b2_bucket_name: @bucket_name,
       b2_bucket_id: @bucket_id,
     )
-    @connection.put_object(@bucket_name, "myfile1", "THISISATESTFILE").json
+    pp @connection.put_object(@bucket_name, "myfile1", "THISISATESTFILE").json
   end
 end
